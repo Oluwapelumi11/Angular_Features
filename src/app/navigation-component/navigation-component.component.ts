d92d3bf178486499d560d6006745157443f547e3
oluwapelumi11 <oluwapelumiezeliel11@gmail.com>
2024-02-03 13:43:20 +0100

    added navigation and app module

---
+++ src/app/navigation-component/navigation-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './navigation-component.component.html',
+  styleUrl: './navigation-component.component.css'
+})
+export class NavigationComponentComponent {
+
+}
