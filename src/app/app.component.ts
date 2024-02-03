7a8e7bf4bc54fbd0b5ed4804987b7e2efe674cad
oluwapelumi11 <oluwapelumiezeliel11@gmail.com>
2024-02-03 13:48:08 +0100

    added navigation and app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component,OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {  RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
+import { NavigationComponentComponent } from './navigation-component/navigation-component.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavigationComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
